c8bc17e7328188419f1a51d7de0183402b6492a1
Quinn <frank.q.gu@gmail.com>
2025-10-07 18:47:40 -0400

    asd

--- supabase.js
+++ supabase.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://jsidasxyybtcziztzehm.supabase.co"
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpzaWRhc3h5eWJ0Y3ppenR6ZWhtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk3NzIyMjIsImV4cCI6MjA3NTM0ODIyMn0.8dDibyEWi1hvd - 2qEWVAejpTsul16Wx87QXmQA1EEO0"
+
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
